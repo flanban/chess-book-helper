dde8864635a4d0ba751f568f0793f52dfa2e924b
flanban <johnsfuller@gmail.com>
2017-06-12 09:29:32 -0400

    misc, styling

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,15 +2,6 @@ class Match < ApplicationRecord
 
   has_and_belongs_to_many :books
 
-  before_validation :strip_input_fields
-
-
-  def strip_input_fields
-    self.attributes.each do |key, value|
-      self[key] = value.strip if value.respond_to?("strip")
-    end
-  end
-
   validates :pgn, presence: true,
                     length: { minimum: 5 }
 end
